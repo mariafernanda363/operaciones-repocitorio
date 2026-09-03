0175d9e60b1e82cdff011b5bceabb5cbdb2f8557
Maria Fernanda <mafesarmientorincon9@gmail.com>
2026-09-03 10:15:16 -0500

    first commit

---
+++ operaciones.py
@@ -0,0 +1,11 @@
+numero1,numero2 = map (int,input("ingrese dos numeros: ").split() )
+
+suma = numero1 + numero2 
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+divicion = numero1 / numero2
+
+print("la suma de los dos numero es:", suma)
+print("la resta de los dos numero es:", resta)
+print("la multiplicacion de los dos numero es:", multiplicacion)
+print("la divicion de los dos numero es:", divicion)
