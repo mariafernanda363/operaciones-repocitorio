4d2b01fad9d3f30247b1feaf202e4a54a8b30e0f
Maria Fernanda <mafesarmientorincon9@gmail.com>
2026-09-03 10:46:32 -0500

    first commit

--- operaciones.py
+++ operaciones.py
@@ -1,11 +1,13 @@
-numero1,numero2 = map (int,input("ingrese dos numeros: ").split() )
+numero1,numero2 = map (int,input("ingrese dos numeros:").split() )
 
 suma = numero1 + numero2 
 resta = numero1 - numero2
 multiplicacion = numero1 * numero2
 divicion = numero1 / numero2
+porcentaje = (numero1 / numero2) * 100
 
 print("la suma de los dos numero es:", suma)
 print("la resta de los dos numero es:", resta)
 print("la multiplicacion de los dos numero es:", multiplicacion)
 print("la divicion de los dos numero es:", divicion)
+print("el porcentaje de los dos numero es:", porcentaje, "%")
